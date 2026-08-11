b6200b87c4bb13ab0b68abdc6e86f2a89222aafd
mishanaer <mikhail.naer@gmail.com>
2026-08-11 10:00:18 +0300

    export Storybook runtime

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export { StorybookShell, default } from "./StorybookShell.jsx";
+export { StorybookShell } from "./StorybookShell.jsx";
+export { StorybookRuntime, StorybookPreview } from "./StorybookRuntime.jsx";
+export {
+  getStorybookIndexUrl,
+  getStorybookPreviewUrl,
+  groupsFromStorybookIndex,
+  normalizeStorybookUrl,
+  readStoryIdFromLocation,
+  storyEntriesFromIndex,
+  writeStoryIdToLocation,
+} from "./storybook-runtime.js";
+export { StorybookRuntime as default } from "./StorybookRuntime.jsx";
